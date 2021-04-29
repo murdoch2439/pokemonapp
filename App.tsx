3985fede1a961c45a605443d70a08d3f77864a88
murdoch2439 <murdochkhallz@gmail.com>
2021-04-29 15:04:55 +0100

    entry point of the application which renders the pokemonList under the statusBar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import PokemonList from './src/screen/pokemon-list';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <PokemonList />
     </View>
   );
 }
@@ -14,8 +15,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding:10
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
