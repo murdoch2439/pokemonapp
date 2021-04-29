e4fcd14bb19fe9aac728e5476561e479c443f9b7
murdoch2439 <murdochkhallz@gmail.com>
2021-04-29 15:11:01 +0100

    detail screen for each pokemon

---
+++ src/screen/pokemon-details.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+import Pokemon from "../model/pokemon";
+
+type Props = {
+  pokemon : Pokemon
+}
+const PokemonDetails : FunctionComponent<Props> = ({pokemon}) =>{
+  return (
+    <></>
+  )
+}
+
+export default PokemonDetails;
