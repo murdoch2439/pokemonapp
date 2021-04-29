b19387598a1b10d36f2a2c3ac4aff8884b02dfaf
murdoch2439 <murdochkhallz@gmail.com>
2021-04-29 15:12:30 +0100

    Pokemon Card component to render in the flatlist

---
+++ src/component/pokemon/pokemon-card.tsx
@@ -0,0 +1,86 @@
+import React, { FunctionComponent } from "react";
+import { Image, View, Text, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import Pokemon from "../../model/pokemon";
+import formatType from "../common/formatType";
+import Helpers from "../common/helpers";
+
+type Props = {
+  pokemon : Pokemon;
+  handlePress: any;
+}
+const PokemonCard : FunctionComponent<Props> = ({pokemon, handlePress}) => {
+
+  return(
+    <>
+    <TouchableWithoutFeedback onPress={handlePress}>
+
+     <View style={styles.container}>
+        <View style={styles.pictureContainer}>
+
+          <Image source={{uri:pokemon.picture}} style={styles.image} />
+        </View>
+        <View style={{padding:10}}>
+          <Text style={styles.title}>{pokemon.name}</Text>
+          {/* <Text>{pokemon.created}</Text> */}
+         <View style={styles.types}>
+
+            {
+            pokemon.types.map((type)=>
+              (<Text style={ formatType(type).type}>{type}</Text>))
+            }
+         </View>
+          
+          <Text>Point de vie</Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+      
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'row',
+    height:200,
+    // width:'90%',
+    backgroundColor:'pink',
+    marginBottom:10,
+    borderRadius:5,
+    // shadowColor:'black',
+    borderWidth:3,
+    borderStyle:'dotted',
+    borderColor:'pink'
+  },
+  pictureContainer:{
+    backgroundColor:'gray',
+    width:'43%',
+    height:'100%',
+    borderTopLeftRadius:5,
+    borderBottomLeftRadius:5
+    // paddingHorizontal:10,
+  },
+  image:{
+    height:'100%',
+    width:'100%',
+    alignSelf:'center'
+
+    
+  },
+  title:{
+    fontSize:30,
+    fontWeight:'bold',
+  },
+  types:{
+    flexDirection:'row',
+    // paddingHorizontal:10
+  },
+  type:{
+    fontSize:18,
+    marginRight:5,
+  }
+})
+
+
+export default PokemonCard;
