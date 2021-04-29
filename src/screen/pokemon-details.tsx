6275d020b4833672a1f8ffc9242f9bd9a2b9c748
murdoch2439 <murdochkhallz@gmail.com>
2021-04-29 15:13:52 +0100

    detail about each pokemon

--- src/screen/pokemon-details.tsx
+++ src/screen/pokemon-details.tsx
@@ -2,11 +2,14 @@ import { FunctionComponent } from "react";
 import Pokemon from "../model/pokemon";
 
 type Props = {
+
   pokemon : Pokemon
 }
 const PokemonDetails : FunctionComponent<Props> = ({pokemon}) =>{
   return (
-    <></>
+    <>
+    
+    </>
   )
 }
 
