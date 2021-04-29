2c9ed62d17c2194cd993addd4d2b072a7f37e0a2
murdoch2439 <murdochkhallz@gmail.com>
2021-04-29 15:11:39 +0100

    form for adding and editing a pokemon infos

---
+++ src/screen/pokemon-edit.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react";
+import Pokemon from "../model/pokemon";
+
+type Props = {
+  pokemon : Pokemon|null
+}
+const PokemonEdit : FunctionComponent<Props> = ({pokemon}) =>{
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default PokemonEdit;
