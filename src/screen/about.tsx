6e4f6a2b84ad18152025e8d953ce73e6a759c10f
murdoch2439 <murdochkhallz@gmail.com>
2021-04-29 17:10:16 +0100

    Adding a text component to the about screen

---
+++ src/screen/about.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+import { Text, View } from "react-native";
+
+
+const AboutScreen : FunctionComponent = () =>{
+ 
+
+  return (
+    
+    <View>
+      <Text>Welcome to about Screen</Text>
+    </View>
+    
+  )
+}
+
+export default AboutScreen;
