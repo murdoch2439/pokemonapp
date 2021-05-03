71250d5f9738c296e4cd51b788f98db39673123c
Benny Nkonga <benny.nkonga@businesslighthouse.tech>
2021-05-03 09:39:25 +0100

    pokemon default value(number/int) set to 0

--- src/model/pokemon.tsx
+++ src/model/pokemon.tsx
@@ -1,8 +1,8 @@
 export default class Pokemon {
  // 1. Typage des propiétés d'un pokémon.
  id: number = -1;
- hp: number = 100;
-  cp: number = 10;
+ hp: number = 0;
+  cp: number = 0;
   name: string = 'name';
   picture: string = 'https://www.maxpixel.net/static/photo/640/Heart-Postcard-Rabbit-Cute-Valentine-3075088.png';
   types: Array<string> = ['Normal'];
@@ -11,11 +11,11 @@ export default class Pokemon {
  // 2. Définition des valeurs par défaut des propriétés d'un pokémon.
  setValues(
   id: number,
-  hp: number = 100,
-  cp: number = 10,
-  name: string = 'name',
-  picture: string = 'http://...',
-  types: Array<string> = ['Normal'],
+  hp: number,
+  cp: number,
+  name: string,
+  picture: string ,
+  types: Array<string>,
   created: Date = new Date()
  ) {
   // 3. Initialisation des propiétés d'un pokémons.
